122742cec251a120306cf9270db92bfec271a9d7
Kamil U <kaurbanski@deloittece.com>
2025-11-03 09:45:33 +0100

    Initial commit: Angular Todo App

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { inject, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule, HttpClientModule,  FormsModule],
+  providers: [],
+})
+export class AppModule {
+  
+}
